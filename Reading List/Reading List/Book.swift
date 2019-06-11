047ab185adba224e51331a939d8ea46559c61267
Donovan Langager <donovan.lan16@gmail.com>
2019-06-11 16:04:00 -0700

    Added boards and connected book and reason for book list

---
+++ Reading List/Reading List/Book.swift
@@ -0,0 +1,33 @@
+//
+//  Book.swift
+//  Reading List
+//
+//  Created by Uptiie on 6/11/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Book {
+   let title: String
+   let reasonToRead: Bool
+   let hasBeenRead: Bool
+    
+    
+}
+
+struct ShippingOptions: OptionSet {
+    let rawValue: Int
+    
+    static let nextDay    = ShippingOptions(rawValue: 1 << 0)
+    static let secondDay  = ShippingOptions(rawValue: 1 << 1)
+    static let priority   = ShippingOptions(rawValue: 1 << 2)
+    static let standard   = ShippingOptions(rawValue: 1 << 3)
+    
+    static let express: ShippingOptions = [.nextDay, .secondDay]
+    static let all: ShippingOptions = [.express, .priority, .standard]
+}
+
+
+
+
